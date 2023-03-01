57967f987630d32d317c22ebd0b05f2310178753
Chohye <cindykim2010@gmail.com>
2023-03-01 22:48:07 +0900

    Feat: Restaurant info scraping and DB init

--- restaurant_init.py
+++ restaurant_init.py
@@ -1,23 +1,54 @@
+import requests
 from pymongo import MongoClient
+from playwright.sync_api import Playwright, sync_playwright
+from bs4 import BeautifulSoup
 
 client = MongoClient('localhost', 27017)
 db = client.jungleroad
 
+url = "https://m.map.naver.com/search2/search.naver?query=kaist%EB%AC%B8%EC%A7%80%EC%BA%A0%ED%8D%BC%EC%8A%A4%EB%A7%9B%EC%A7%91&sm=sug&style=v5#/list"
 
-def restaurantsInit():
-    db.restaurants.insert_one({'name': '한우곰탕', 'rating': 4.55, 'junglerating': 0, 'category': '소고기구이', 'images': [
-                              'https://search.pstatic.net/common/?autoRotate=true&type=w278_sharpen&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20191023_189%2F15717946190105vktc_JPEG%2FLFPvlJWAUHpZwYcHx5C6igJw.jpg'], 'address': '대전 유성구 전민로22번길 30', 'contact': '042-861-3313'})
-    db.restaurants.insert_one({'name': '화목한우리집', 'rating': 4.57, 'junglerating': 0, 'category': '종합분식', 'images': [
-                              'https://search.pstatic.net/common/?autoRotate=true&type=w278_sharpen&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220913_111%2F1663073555626lDK1T_JPEG%2FD036DE7C-5D30-4B8C-849F-EC42AC7A4EEF.jpeg'], 'address': '대전 유성구 전민로 64', 'contact': '042-862-0282'})
-    db.restaurants.insert_one({'name': '광세족발 본점', 'rating': 4.51, 'junglerating': 0, 'category': '족발, 보쌈', 'images': [
-                              'https://search.pstatic.net/common/?autoRotate=true&type=w278_sharpen&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220913_111%2F1663073555626lDK1T_JPEG%2FD036DE7C-5D30-4B8C-849F-EC42AC7A4EEF.jpeg'], 'address': '대전 유성구 전민로22번길 51', 'contact': '042-863-6142'})
-    db.restaurants.insert_one({'name': '국영수떡볶이', 'rating': 4.69, 'junglerating': 0, 'category': '떡볶이', 'images': [
-                              'https://search.pstatic.net/common/?autoRotate=true&type=w278_sharpen&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180715_30%2F15316329892524l4MN_JPEG%2FNUxPsjFarV4cP8lL2LyZUTo3.JPG.jpg'], 'address': '대전 유성구 전민로 62 1층(국영수떡볶이)', 'contact': '042-331-9825'})
-    db.restaurants.insert_one({'name': '스시안', 'rating': 4.42, 'junglerating': 0, 'category': '초밥, 롤', 'images': [
-                              'https://search.pstatic.net/common/?autoRotate=true&type=w278_sharpen&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190324_90%2F1553355821287FJRSJ_JPEG%2FiD7jYUL3obP01Odk1sSEP1eZ.JPG.jpg'], 'address': '대전 유성구 전민로70번길 27', 'contact': '042-861-5084'})
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=True)
+    page = browser.new_page()
+    # page.set_viewport_size({"width" : 800, "height" : 600})
+    page.goto(url)
+    # page.wait_for_selector("div.item_info", timeout=50000)
+    page.wait_for_timeout(50000)
+    loaded_html = page.content()
+    soup = BeautifulSoup(loaded_html, "html.parser")
+    browser.close() 
 
+restaurants = soup.select('li._lazyImgContainer')
 
-restaurantsInit()
+for restaurant in restaurants:
+    data_id = restaurant.attrs['data-id']
+    contact = restaurant.attrs['data-tel']
+    url = 'https://m.place.naver.com/restaurant/' + data_id + '/home'
+    name = restaurant.select_one('div.item_info > a.a_item.a_item_distance._linkSiteview > div > strong').text
+    category = restaurant.select_one('div.item_info > a.a_item.a_item_distance._linkSiteview > div > em').text
+    address = restaurant.select_one('div.item_info > div.item_info_inn > div > a').text.replace("주소보기","").replace(" ","")
+    # img = restaurant.select_one('div.item_info > a.item_thumb._itemThumb > img').attrs['src']
 
+    print(data_id)
+    print(contact)
+    print(url)
+    print(name)
+    print(category)
+    print(address)
+
+    doc = {
+        'data_id' : data_id,
+        'name' : name,
+        'category' : category,
+        'rating' : 0,
+        'junglerating' : 0,
+        'images' : [],
+        'address' : address,
+        'contact' : contact, 
+        'url' : url
+    }
+
+    db.restaurants.insert_one(doc)
 
 print("DB init success")
